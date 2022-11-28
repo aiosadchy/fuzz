2127de5fe1f6d966361f3b20ed73b446e9876d35
Alexander Osadchy <a.i.osadchy@gmail.com>
2022-11-29 01:43:32 +0300

    add templates for string splitting function

--- include/fuzz/fuzz.h
+++ include/fuzz/fuzz.h
@@ -4,38 +4,54 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct FuzzConfig {
     struct {
         size_t initial_buffer_size;
         double buffer_growth_rate;
-    } file_input;
+    } raw_data_reading;
+
+    struct {
+        size_t initial_buffer_size;
+        double buffer_growth_rate;
+    } string_array_building;
+
 } FuzzConfig;
+
+typedef struct FuzzRawData {
+    char *data;
+    size_t size;
+    size_t allocation_size;
+} FuzzRawData;
 
 typedef struct FuzzString {
     const char *begin;
     const char *end;
 } FuzzString;
 
-typedef struct FuzzRawData {
-    const char *data;
-    size_t size;
-    size_t allocation_size;
-} FuzzRawData;
-
-typedef struct FuzzData {
+typedef struct FuzzStringArray {
     FuzzString *data;
     size_t size;
     size_t allocated_size;
-} FuzzData;
+} FuzzStringArray;
 
 FuzzConfig fuzz_make_config(void);
 FuzzConfig fuzz_get_config(void);
 FuzzConfig *fuzz_get_config_location(void);
 void fuzz_set_config(FuzzConfig config);
 
-int fuzz_read_from_file(FuzzRawData *data, FILE *file);
+FuzzRawData fuzz_make_raw_data(void);
+int fuzz_read_raw_data(FuzzRawData *data, FILE *file);
 void fuzz_clear_raw_data(FuzzRawData *data);
+
+FuzzStringArray fuzz_make_string_array(void);
+int fuzz_build_string_array(
+    FuzzStringArray *data,
+    const FuzzRawData *raw_data,
+    const char *separator
+);
+void fuzz_clear_string_array(FuzzStringArray *data);
 
 
 #ifdef FUZZ_IMPLEMENTATION
@@ -43,7 +59,11 @@ void fuzz_clear_raw_data(FuzzRawData *data);
 FuzzConfig fuzz_make_config(void) {
     // clang-format off
     FuzzConfig result = {
-        .file_input = {
+        .raw_data_reading = {
+            .initial_buffer_size = 4096,
+            .buffer_growth_rate = 2.0,
+        },
+        .string_array_building = {
             .initial_buffer_size = 4096,
             .buffer_growth_rate = 2.0,
         },
@@ -70,22 +90,35 @@ void fuzz_set_config(FuzzConfig config) {
     *fuzz_get_config_location() = config;
 }
 
-int fuzz_read_from_file(FuzzRawData *data, FILE *file) {
+FuzzRawData fuzz_make_raw_data(void) {
+    FuzzRawData raw_data = {
+        .data            = NULL,
+        .size            = 0,
+        .allocation_size = 0,
+    };
+    return raw_data;
+}
+
+int fuzz_read_raw_data(FuzzRawData *data, FILE *file) {
     if (data == NULL || file == NULL) {
         return 0;
     }
 
-    const FuzzConfig config = fuzz_get_config();
+    const FuzzConfig config          = fuzz_get_config();
+    const size_t initial_buffer_size = config.raw_data_reading.initial_buffer_size;
+    const double buffer_growth_rate  = config.raw_data_reading.buffer_growth_rate;
 
     while (!feof(file)) {
         size_t free_space = data->allocation_size - data->size;
 
         if (free_space == 0) {
-            size_t new_size = (data->allocation_size == 0)
-                ? config.file_input.initial_buffer_size
-                : (size_t)((double)data->size * config.file_input.buffer_growth_rate);
+            // clang-format off
+            const size_t new_size = (data->allocation_size == 0)
+                ? initial_buffer_size
+                : (size_t)((double)data->size * buffer_growth_rate);
+            // clang-format on
 
-            const char *buffer = realloc((void *)(data->data), new_size);
+            char *buffer = realloc((void *)(data->data), new_size);
             if (buffer == NULL) {
                 return -1;
             }
@@ -94,7 +127,7 @@ int fuzz_read_from_file(FuzzRawData *data, FILE *file) {
             free_space            = data->allocation_size - data->size;
         }
 
-        size_t characters_read = fread(
+        const size_t characters_read = fread(
             (void *)(data->data + data->size),
             sizeof(*data->data),
             free_space,
@@ -117,6 +150,43 @@ void fuzz_clear_raw_data(FuzzRawData *data) {
         data->data            = NULL;
         data->size            = 0;
         data->allocation_size = 0;
+    }
+}
+
+FuzzStringArray fuzz_make_string_array(void) {
+    FuzzStringArray data = {
+        .data           = NULL,
+        .size           = 0,
+        .allocated_size = 0,
+    };
+    return data;
+}
+
+int fuzz_build_string_array(
+    FuzzStringArray *data,
+    const FuzzRawData *raw_data,
+    const char *separator
+) {
+    if (data == NULL || raw_data == NULL) {
+        return 0;
+    }
+
+    const size_t separator_length = (separator == NULL) ? 0 : strlen(separator);
+    if (separator_length == 0) {
+        return 0;
+    }
+
+    // TODO: implementation
+
+    return 0;
+}
+
+void fuzz_clear_string_array(FuzzStringArray *data) {
+    if (data != NULL) {
+        free((void *)(data->data));
+        data->data           = NULL;
+        data->size           = 0;
+        data->allocated_size = 0;
     }
 }
 
--- test/src/main.c
+++ test/src/main.c
@@ -2,22 +2,18 @@
 #include <fuzz/fuzz.h>
 
 int main(void) {
-    FuzzRawData raw_data = {
-        .data            = NULL,
-        .size            = 0,
-        .allocation_size = 0,
-    };
+    FuzzRawData raw_data = fuzz_make_raw_data();
 
     FILE *f1 = fopen("/home/aiosadchy/1.txt", "r");
     FILE *f2 = fopen("/home/aiosadchy/2.txt", "r");
 
-    fuzz_read_from_file(&raw_data, f1);
-    fuzz_read_from_file(&raw_data, f2);
+    fuzz_read_raw_data(&raw_data, f1);
+    fuzz_read_raw_data(&raw_data, f2);
 
     fclose(f1);
     fclose(f2);
 
-    fuzz_get_config_location()->file_input.initial_buffer_size = 10;
+    fuzz_get_config_location()->raw_data_reading.initial_buffer_size = 10;
 
     printf("size            = [%zu]\n", raw_data.size);
     printf("allocation_size = [%zu]\n", raw_data.allocation_size);
